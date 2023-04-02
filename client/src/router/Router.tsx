acf613c1a918ecbf7c8d9280a7a52339bdf21807
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-04-02 13:54:08 +0200

    Updated router

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,17 +1,20 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Test from "../pages/Test";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/index";
+import HomePage from "../pages/HomePage";
+import JobPage from "../pages/JobPage";
+import AuthPage from "../pages/AuthPage";
 
 const Router = () => {
-  //used to protect routes
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/job/:jobId" element={<JobPage />} />
+        {!isAuthenticated && <Route path="/auth" element={<AuthPage />} />}
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
